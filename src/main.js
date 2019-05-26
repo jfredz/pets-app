9d8d00e32111b49b1a7ad411357baba4aea095ef
Cristian <mlcristianv@gmail.com>
2019-05-26 20:44:30 +0300

    implement pets route

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faCat } from '@fortawesome/free-solid-svg-icons'
+import { faDog } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faCat)
+library.add(faDog)
+
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
@@ -9,6 +17,7 @@ import router from './router'
 import store from './store'
 
 Vue.use(BootstrapVue)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
--- src/views/Pet.vue
+++ src/views/Pet.vue
@@ -1,5 +1,32 @@
 <template>
 	<div>
-		{{ $route.params.id }}
+		<h1>
+			<font-awesome-icon :icon="this.$route.params.species === 'cats' ? 'cat' : 'dog'" />
+			{{ pet.name }}	
+		</h1>
+		
 	</div>
+
 </template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+	data() {
+		return {
+			pet: {}
+		}
+	},
+  computed: {
+    ...mapState([
+      'cats',
+      'dogs'
+    ])
+  },
+  mounted() {
+  		const pet = this[this.$route.params.species][this.$route.params.id];
+  		this.pet = pet;
+  }
+}
+</script>
